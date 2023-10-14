52c0ea049c7de4196398701d5830170b102698b7
RafaelJohn9 <rafaeljohb@gmail.com>
2023-10-14 14:33:59 +0300

    added comments, pycodestyle, corrected typo in square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 
+"""
+this is a class square
+"""
+
+
 class square():
-    
+    """ this is given its attr here """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ the attr given to the class """
         for key, value in kwargs.items():
             setattr(self, key, value)
         self.height = self.width
@@ -15,15 +20,17 @@ class square():
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def PerimiterOfMySquare(self):
+        """ Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ what is printed out when class is printed out """
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.PerimiterOfMySquare())
