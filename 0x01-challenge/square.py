0369fff65da399071707334f0e5fc526924f2785
RafaelJohn9 <rafaeljohb@gmail.com>
2023-10-14 15:21:44 +0300

    conventional method naming

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -20,7 +20,7 @@ class Square():
         """ Area of the square """
         return self.height * self.width
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
         """ Permiter of my square """
         return (self.width * 2) + (self.height * 2)
 
@@ -33,4 +33,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
