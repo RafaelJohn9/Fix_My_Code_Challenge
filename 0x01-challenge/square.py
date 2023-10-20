69b0b40d69156b30f7d36594321c8d562c4cfe16
RafaelJohn9 <rafaeljohb@gmail.com>
2023-10-20 13:34:15 +0300

    class square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -20,7 +20,7 @@ class Square():
         """ Area of the square """
         return self.height * self.width
 
-    def perimeter_of_my_square(self):
+    def permiter_of_my_square(self):
         """ Permiter of my square """
         return (self.width * 2) + (self.height * 2)
 
@@ -30,6 +30,7 @@ class Square():
 
 
 if __name__ == "__main__":
+    """ script to be runned from an unimported module """
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
